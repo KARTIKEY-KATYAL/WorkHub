1be0817ce8ab343dae7b2f8fc2d1759042f256f9
KARTIKEY-KATYAL <107783109+KARTIKEY-KATYAL@users.noreply.github.com>
2024-12-21 20:53:00 +0530

    organisatiions added

--- app/(routes)/dashboard/(components)/Header.jsx
+++ app/(routes)/dashboard/(components)/Header.jsx
@@ -1,24 +1,28 @@
-import { UserButton } from "@clerk/nextjs";
+"use client"
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import React from "react";
-import Logo from "../components/Logo";
+import Image from "next/image";
+import { useAuth } from "@clerk/clerk-react";
 
 const Header = () => {
+    const {orgId} = useAuth()
+    console.log(orgId)
   return (
     <div className="w-full flex items-center justify-between px-4 py-2 bg-slate-100 shadow-md">
       {/* Left Section: Logo */}
-      <Logo />
+      <div className="flex items-center space-x-2">
+        <Image src="/logo.png" alt="WorkHub Logo" width={30} height={30} />
+        <span className="text-2xl font-semibold text-gray-800 kanit-bold ">
+          WorkHub
+        </span>
+      </div>
 
       {/* Middle Section: Navigation Links */}
       <div className="hidden md:flex space-x-4">
-        <a href="#features" className="text-gray-600 hover:text-gray-800">
-          Features
-        </a>
-        <a href="#pricing" className="text-gray-600 hover:text-gray-800">
-          Pricing
-        </a>
-        <a href="#about" className="text-gray-600 hover:text-gray-800">
-          About
-        </a>
+            <OrganizationSwitcher
+            afterLeaveOrganizationUrl={"/dashboard"}
+            afterSelectOrganizationUrl={"/dashboard"}
+            />
       </div>
 
       {/* Right Section: User Button */}
